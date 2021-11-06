fc92e75cdb4fac979d701daf49d46aec093f5d48
todak-sun <tjsdydwn@gmail.com>
2021-11-06 22:02:12 +0900

    add

--- aisApiDataCollector/jest.config.js
+++ aisApiDataCollector/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleDirectories: ['node_modules'],
-  testMatch: ['<rootDir>/**/*.test.(js|jsx|ts|tsx)'],
+  // testMatch: ['<rootDir>/**/*.test.(js|jsx|ts|tsx)'],
   testPathIgnorePatterns: ['node_modules'],
   moduleFileExtensions: ['js', 'ts', 'json'],
   verbose: this.transform,
@@ -12,7 +12,7 @@ module.exports = {
     '@/(.*)': '<rootDir>/src/$1',
   },
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   rootDir: '.',
 };
